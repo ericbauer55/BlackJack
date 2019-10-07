27a2c8384fb70510ff7cb6dd1ad095a40c546c49
vulcant <eric.bauer@gmail.com>
2019-10-07 09:38:02 -0400

    created a blackjack class

---
+++ src/Blackjack.py
@@ -0,0 +1,18 @@
+import random
+from src.class_defs.chip_stack import ChipStack
+from src.class_defs.cards import Card, CardPile, CardHand
+from src.class_defs.players import Player, get_valid_input
+
+
+class BlackJack:
+    # =========== Constructors ===========
+    def __init__(self):
+        self.dealer = Player()
+
+    # =========== Helper Methods ===========
+
+    # =========== Game Actions ===========
+
+    # =========== Control Flow Actions ===========
+
+
