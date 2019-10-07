ff07f9a331ddbd330b171b4658ae7e97ebb9c680
vulcant <eric.bauer@gmail.com>
2019-10-07 09:45:08 -0400

    successfully instantiate the dealer

--- src/Blackjack.py
+++ src/Blackjack.py
@@ -2,12 +2,14 @@ import random
 from src.class_defs.chip_stack import ChipStack
 from src.class_defs.cards import Card, CardPile, CardHand
 from src.class_defs.players import Player, get_valid_input
+from typing import Dict, List, Optional
 
 
 class BlackJack:
     # =========== Constructors ===========
     def __init__(self):
-        self.dealer = Player()
+        self.dealer = Player(name='dealer', chips=ChipStack.from_dealer_stack())
+        self.dealer.chips.view_stack()
 
     # =========== Helper Methods ===========
 
@@ -16,3 +18,5 @@ class BlackJack:
     # =========== Control Flow Actions ===========
 
 
+if __name__ == '__main__':
+    game = BlackJack()
