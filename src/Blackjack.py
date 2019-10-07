cdd79d1e3154b2d433a4dec2d321c27a3f58707b
vulcant <eric.bauer@gmail.com>
2019-10-07 10:52:24 -0400

    created the basic initialization function for a hand as well as two critical game action placeholders

--- src/Blackjack.py
+++ src/Blackjack.py
@@ -12,9 +12,12 @@ class BlackJack:
         self.dealer: Player = Player(name='dealer', chips=ChipStack.from_dealer_stack())
         self.dealer.chips.view_stack()
         self.players: Dict[str, Player] = {'human': Player('human', ChipStack.from_standard_stack())}
+        self.dealt_in_players: List[str] = list(self.players.keys())  # deal-in all players initially
         # Setup the Decks
         self.draw_pile: CardPile = CardPile.from_standard_deck()
+        self.draw_pile.shuffle()
         self.discard_pile: CardPile = CardPile()
+
 
     # =========== Helper Methods ===========
     @staticmethod
@@ -38,8 +41,35 @@ class BlackJack:
         if buy_in_key not in ChipStack.get_empty_stack().keys():
             raise KeyError('The buy-in value of \'{}\' is not in the standard denominations'.format(buy_in_key))
         # first check if all players want to buy-in to the hand
-        for player_name, player_obj in self.players.items():
-            pass
+        self.dealt_in_players = list(self.players.keys())  # deal in all players initially
+        self.take_bets(min_bet=buy_in)
+        # second deal hands to all players that are still dealt-in
+        self.deal_cards()
+        # lastly check for any naturals or busts before moving to game loop
+        for player_name in self.dealt_in_players:
+            if BlackJack.is_blackjack(self.players[player_name]):
+                # Dealer pays out to the player
+                # Remove player from dealt_in list
+                pass
+            if BlackJack.is_bust(self.players[player_name]):
+                # Player pays out to dealer
+                # Remove player from dealt_in list
+                pass
+
+
+    def take_bets(self, min_bet: int = 1) -> None:
+        """This function checks if each dealt-in player wants to place a bet, with some minimum imposed"""
+        for player_name, player_obj in self.players.items():  # the dealer doesn't have to buy-in; they are the house
+            # check if player even has the buy-in amount
+            valid_bets: List[str]
+
+    def deal_cards(self, players: List[Player], n_cards: int = 1):
+        """
+        This function deals :param n_cards to each player in the list :param players
+        If the draw pile doesn't have enough cards left in it for all players, the remainder will be dealt
+        and the discard pile will be shuffled in.
+        """
+        pass
 
     # =========== Control Flow Actions ===========
     # These are the functions that solicit user input and control the order of game operations
